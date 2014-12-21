e3cbae52f45ddb604394fd3bb154729629258444
Thomas Wickham <thomas@wickham.epimeros.org>
2014-12-21 21:57:38 +0100

    Disable crsf (4)

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,7 @@
 class LogsController < ApplicationController
+  protect_from_forgery :reset_session, except: :new
+  skip_before_action :verify_authenticity_token
+
   def index
     logs = Log.all.to_json
     render text: "List of logs. LOL<br>#{logs}"
